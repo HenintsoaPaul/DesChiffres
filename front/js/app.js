cb2328e21110c1b2dd83bde2cd910a8c4e698e5e
tsoa <henintsoapaul@gmail.com>
2024-06-12 14:56:55 +0300

    Get winner.

--- front/js/app.js
+++ front/js/app.js
@@ -1,10 +1,15 @@
 const app = angular.module("DesChiffresApp", ["ngRoute"]);
 
 app.controller("mainCtrl", ['$scope', '$http', function ($scope, $http) {
+    $scope.isTimeUp = false;
+    $scope.showJustify = false;
     $scope.numbers = [];
-    $scope.seconds_length = 5;
+    $scope.seconds_length = 3;
     $scope.p1Validation = null;
     $scope.p2Validation = null;
+    $scope.willJustify = null; // The player that will justify.
+    $scope.idWinner = null;
+    $scope.sumJustification = null;
 
     // Get numbers from C# API
     $scope.getNumbers = function () {
@@ -40,24 +45,31 @@ app.controller("mainCtrl", ['$scope', '$http', function ($scope, $http) {
             timer.textContent = `${minutes}:${seconds}`;
             if (remainingTime <= 0) {
                 clearInterval(intervalId);
-                timer.textContent = '00:00';
+                timer.textContent = 'Time is Up';
                 $scope.doSomethingAtZero();
             }
         }, 1000);
     };
 
     // When Timer 00:00
-    const inputP1 = document.getElementById("nbP1");
-    const inputP2 = document.getElementById("nbP2");
     $scope.doSomethingAtZero = function () {
         console.log('Temps écoulé!');
 
-        $scope.getInputs();
+        $scope.$apply(function () {
+            $scope.isTimeUp = true;
+            console.log("Is time up: ", $scope.isTimeUp);
 
-        $scope.getWinner();
+            $scope.getInputs();
+            $scope.getWinner();
+        })
 
         $scope.p1Validation = null;
         $scope.p2Validation = null;
+        $scope.willJustify = null;
+        $scope.sumJustification = null;
+        $scope.idWinner = null;
+        $scope.isTimeUp = false;
+        $scope.showJustify = false;
     };
 
     // Set Values on Validation
@@ -83,6 +95,8 @@ app.controller("mainCtrl", ['$scope', '$http', function ($scope, $http) {
     });
 
     // Get Inputs
+    const inputP1 = document.getElementById("nbP1");
+    const inputP2 = document.getElementById("nbP2");
     $scope.getInputs = function () {
         if ($scope.p1Validation === null) {
             $scope.p1Validation = {
@@ -102,25 +116,54 @@ app.controller("mainCtrl", ['$scope', '$http', function ($scope, $http) {
     $scope.getWinner = function () {
         const nbp1 = $scope.p1Validation.nb;
         const nbp2 = $scope.p2Validation.nb;
-
-        const t1 = $scope.p1Validation.time;
-        const t2 = $scope.p2Validation.time;
-
         console.log(`p1: ${nbp1} and p2: ${nbp2}`);
-        console.log(`t1: ${t1} and t2: ${t2}`);
 
         if (nbp1 === -1 && nbp2 === -1) {
-            console.log("No Winner");
+            console.log("No one submit... You are LOSERS.");
+            $scope.willJustify = null;
         }
-
         else if (nbp1 === -1 && nbp2 !== -1) {
-            console.log("Winner 2");
+            console.log("Player 2 submit");
+            $scope.willJustify = 2;
         }
-
         else if (nbp1 !== -1 && nbp2 === -1) {
-            console.log("Winner 1");
+            console.log("Player 1 submit");
+            $scope.willJustify = 1;
+        }
+        else {
+            console.log("Both submits");
+            const t1 = $scope.p1Validation.time;
+            const t2 = $scope.p2Validation.time;
+            console.log(`t1: ${t1} and t2: ${t2}`);
+
+            // Get chiffres le plus proche de Guess
+            const nbGuess = $scope.numbers['nbGuess'];
+            const decal1 = Math.abs(nbGuess - nbp1);
+            const decal2 = Math.abs(nbGuess - nbp2);
+            if (decal1 < decal2) {
+                console.log("player 1 submits the nearest");
+                $scope.willJustify = 1;
+            }
+            else if (decal1 > decal2) {
+                console.log("player 2 submits the nearest");
+                $scope.willJustify = 2;
+            }
+            else if (decal1 === decal2) {
+                console.log("player 1 and 2 submits numbers at the same distance");
+                if (t1 > t2) {
+                    console.log("player 1 submits first");
+                    $scope.willJustify = 1;
+                }
+                else if (t1 < t2) {
+                    console.log("player 2 submits first");
+                    $scope.willJustify = 2;
+                }
+            }
         }
 
+        if ($scope.willJustify === null) {
+            console.log("No Winner... You are LOSERS.");
+        }
         else {
             $scope.justify();
             $scope.verifyJustification();
@@ -169,6 +212,21 @@ app.controller("mainCtrl", ['$scope', '$http', function ($scope, $http) {
             console.log(`Player ${idPlayer} is right. Winner is ${$scope.idWinner}.`);
             console.log("Both validates");
         }
+        else {
+            $scope.idWinner = $scope.getOpponent(idPlayer);
+
+            console.log(`Player ${idPlayer} is wrong. Winner is ${$scope.idWinner}.`);
+        }
+
+        // Add winner point.
+        // ...
+    }
+
+    // Get Opponent
+    $scope.getOpponent = function (idPlayer) {
+        if (idPlayer === 1) return 2;
+        if (idPlayer === 2) return 1;
+        return -1;
     }
 }]);
 
