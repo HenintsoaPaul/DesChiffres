815ac3d4c579c8f2fa4fedf32425c9b14eb8147f
tsoa <henintsoapaul@gmail.com>
2024-06-28 09:49:38 +0300

    get solution from the API

--- front/js/apiService.js
+++ front/js/apiService.js
@@ -6,7 +6,35 @@ app.service('apiService', ['$http', function ($http) {
         }).then(function(response) {
             const data = response.data;
             console.log(data);
-        });
+
+        // Interpretation des resultats
+            const listOperators = this.getListOperators(data);
+            console.log(`List operators: ${listOperators}`);
+
+            const firstOp = data[0];
+            let operationStr =  `${firstOp[1]} ${this.getOperator(listOperators[0])} ${firstOp[2]}`;
+            for (let i = 1; i < data.length; i++) {
+                operationStr += ` ${this.getOperator(listOperators[i])} ${data[i][2]}`;
+            }
+
+            console.log("operation str: " + operationStr);
+            console.log("sum = " + eval(operationStr))
+        }.bind(this));
+    }
+
+    this.getListOperators = function (data) {
+        let listOperators = [];
+        for (let i = 0; i < data.length; i ++ ) listOperators.push(data[i][3]);
+        return listOperators;
+    }
+
+    this.getOperator = function (idOperator) {
+        switch (idOperator) {
+            case 0: return "*";
+            case 1: return "+";
+            case 2: return "-";
+            case 3: return "/";
+        }
     }
 
     this.sendData = function(nbGuess, nbTools) {
@@ -14,14 +42,14 @@ app.service('apiService', ['$http', function ($http) {
             NbGuess: nbGuess,
             NbTools: nbTools
         };
-        console.log(jsonData);
+        console.log(`Sending json: ${jsonData}`);
 
         $http({
             method: 'POST',
             url: 'http://localhost:5138/Data/set',
             data: jsonData // Include the data here
         }).then(function(response) {
-            console.log("Success", response);
+            console.log("Numbers sent successfully: ", response);
         }, function(error) {
             console.error("Error", error);
         });
